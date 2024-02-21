4d6f519877fc37a85cc83fb514444c9f9857d461
mtiann3 <120132854+mtiann3@users.noreply.github.com>
2024-02-21 13:54:20 -0600

    added forgot password method

---
+++ athleteiq-ios/Core/Authentication/View/ForgotPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordView.swift
+//  athleteiq-ios
+//
+//  Created by Mike Iannotti on 2/20/24.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
