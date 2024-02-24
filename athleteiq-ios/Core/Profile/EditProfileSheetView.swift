af5df13d114350a7438dc2ca30f7216f7cc6e876
mtiann3 <120132854+mtiann3@users.noreply.github.com>
2024-02-24 14:11:19 -0600

    Started edit profile functionality

---
+++ athleteiq-ios/Core/Profile/EditProfileSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileSheetView.swift
+//  athleteiq-ios
+//
+//  Created by Mike Iannotti on 2/24/24.
+//
+
+import SwiftUI
+
+struct EditProfileSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileSheetView()
+}
